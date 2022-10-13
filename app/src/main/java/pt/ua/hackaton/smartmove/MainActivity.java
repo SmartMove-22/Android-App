61753d88f17ee5f61262f6008cf2991e70145184
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2022-10-13 19:47:57 +0100

    Feat: Finished fragment design for coach - Add Exercise.

--- app/src/main/java/pt/ua/hackaton/smartmove/MainActivity.java
+++ app/src/main/java/pt/ua/hackaton/smartmove/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             } else if (item.getItemId() == R.id.statistics_menu_item) {
                 fragmentManager.beginTransaction()
-                        .replace(R.id.fragmentContainerView, ReportFragment.class, null)
+                        .replace(R.id.fragmentContainerView, CoachAddExercisesFragment.class, null)
                         .setReorderingAllowed(true)
                         .addToBackStack("report_fragment")
                         .commit();
