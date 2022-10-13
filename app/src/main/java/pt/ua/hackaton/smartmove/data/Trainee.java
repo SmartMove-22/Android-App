44261812bac3532f2ccc8963ffea2ae69864e843
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2022-10-13 16:24:58 +0100

    Feat: Finished fragment design for coach - Trainees fragment.

--- app/src/main/java/pt/ua/hackaton/smartmove/data/Trainee.java
+++ app/src/main/java/pt/ua/hackaton/smartmove/data/Trainee.java
@@ -2,13 +2,14 @@ package pt.ua.hackaton.smartmove.data;
 
 import java.util.List;
 
-public class Trainee {
+public class Trainee extends User {
 
     private final Coach coach;
     private final List<AssignedExercise> exercises;
     private final List<Report> reports;
 
-    public Trainee(Coach coach, List<AssignedExercise> exercises, List<Report> reports) {
+    public Trainee(String username, String email, String password, String image, Coach coach, List<AssignedExercise> exercises, List<Report> reports) {
+        super(username, email, password, image);
         this.coach = coach;
         this.exercises = exercises;
         this.reports = reports;
